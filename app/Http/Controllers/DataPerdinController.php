4316829545eba4d4a9c3b4959c9ef22d76a39db6
Hikuroshi <92168188+Hikuroshi@users.noreply.github.com>
2024-02-05 13:26:25 +0700

    update

--- app/Http/Controllers/DataPerdinController.php
+++ app/Http/Controllers/DataPerdinController.php
@@ -260,9 +260,15 @@ class DataPerdinController extends Controller
                 $query->where('tersedia', 1);
             })->get();
         } else {
-            $pegawais = Pegawai::whereNotNull('golongan_id')->where('bidang_id', $authBidangId)->whereHas('ketentuan', function ($query) {
+            $kabid = Pegawai::where('nama', 'like', '%Kepala Bidang%')->get();
+            $sekdis = Pegawai::where('nama', 'like', '%Sekretaris Dinas%')->get();
+            $pegawai = Pegawai::whereNotNull('golongan_id')
+            ->where('bidang_id', $authBidangId)
+            ->whereHas('ketentuan', function ($query) {
                 $query->where('tersedia', 1);
             })->get();
+
+            $pegawais = $pegawai->merge($kabid)->merge($sekdis);
         }
 
         $selectedPegawai = collect();
