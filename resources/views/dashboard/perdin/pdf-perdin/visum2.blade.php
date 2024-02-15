ad327990d8ee893a00e58dfb3d568fbc40ad227a
Hikuroshi <hikulana04@gmail.com>
2024-02-15 16:39:08 +0700

    update

--- resources/views/dashboard/perdin/pdf-perdin/visum2.blade.php
+++ resources/views/dashboard/perdin/pdf-perdin/visum2.blade.php
@@ -49,8 +49,8 @@
 					<tr>
 						<td></td>
 						<td colspan="2">
-							<p style="text-decoration: underline; font-weight: bold; margin-top: 60px">{{ $data_perdin->pptk->nama }}</p>
-							<p>NIP.{{ $data_perdin->pptk->nip }}</p>
+							<p style="text-decoration: underline; font-weight: bold; margin-top: 60px">{{ $data_perdin->pptk->nama ?? '-' }}</p>
+							<p>NIP.{{ $data_perdin->pptk->nip }} ?? '-</p>
 						</td>
 					</tr>
 				</table>
