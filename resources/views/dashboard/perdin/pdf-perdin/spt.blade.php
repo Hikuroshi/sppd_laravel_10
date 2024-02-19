e8b7a445e9bad4e87ef1c7c2732148b49d407190
Hikuroshi <hikulana04@gmail.com>
2024-02-19 10:46:35 +0700

    update

--- resources/views/dashboard/perdin/pdf-perdin/spt.blade.php
+++ resources/views/dashboard/perdin/pdf-perdin/spt.blade.php
@@ -136,7 +136,7 @@
 						<div style="display: inline-block; text-align: left;">
 							<p style="margin-top: 20px;">
 								Serang,  {{ Carbon\Carbon::parse($data_perdin->tgl_berangkat)->isoFormat('D MMMM YYYY') }} <br>
-								Kepala Dinas
+								{{ explode(' ', $data_perdin->tanda_tangan->pegawai->jabatan->nama, 3) }}
 							</p>
 							<img src="data:image/png;base64,{{ $data_perdin->tanda_tangan->fileTtdEncoded }}" alt="{{ $data_perdin->tanda_tangan->nama }}" height="70">
 							<p style="text-decoration: underline; font-weight: bold;">{{ $data_perdin->tanda_tangan->pegawai->nama }}</p>
